c033b679ca1d22b0800fc96921630eb8cc580400
BabyStu <srw33de@gmail.com>
2024-02-07 14:34:35 -0600

    updates new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="footer center">
-            <p className='center smallBorderBox highlight'>&copy; 2024</p>
+            <div className='center smallBorderBox highlight'>&copy;2024</div>
         </footer>
     );
 }
